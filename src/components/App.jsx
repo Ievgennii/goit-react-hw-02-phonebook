ef69acac086f9df4af03c1c08aabcb7bbc4f29d6
Ievgennii <simonovievgenii@gmail.com>
2023-05-12 00:14:31 +0300

    add local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,13 +9,28 @@ import Filter from './Filter';
 class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts')
+    const parseContacts = JSON.parse(contacts)
+    if(parseContacts) {
+      this.setState({contacts: parseContacts})
+    }
+    
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== this.prevState) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   deleteContact = contactId => {
     this.setState(prevState => ({
